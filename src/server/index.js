bfd3bf2400ec04bcb858cb12da267de55a8faa22
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-03-09 19:51:48 +0200

    nlp

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+const dotenv = require("dotenv");
+dotenv.config();
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
@@ -21,3 +23,20 @@ app.listen(8080, function() {
 app.get("/test", function(req, res) {
   res.send(mockAPIResponse);
 });
+
+var AYLIENTextAPI = require("aylien_textapi");
+var textapi = new AYLIENTextAPI({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
+
+textapi.sentiment(
+  {
+    text: "John is a very good football player!"
+  },
+  function(error, response) {
+    if (error === null) {
+      console.log(response);
+    }
+  }
+);
