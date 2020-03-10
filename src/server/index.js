8ffb5660e7df6ab8a21f545c08904a9a546228eb
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-03-10 12:09:15 +0200

    initializing form handler function

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,12 @@ dotenv.config();
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
-
+const APIrespone = {
+  polarity: "default",
+  subjectivity: "default"
+};
 const app = express();
-
 app.use(express.static("dist"));
-
-console.log(__dirname);
 
 app.get("/", function(req, res) {
   // res.sendFile('dist/index.html')
@@ -20,23 +20,26 @@ app.listen(8080, function() {
   console.log("Example app listening on port 8080!");
 });
 
-app.get("/test", function(req, res) {
-  res.send(mockAPIResponse);
-});
-
 var AYLIENTextAPI = require("aylien_textapi");
 var textapi = new AYLIENTextAPI({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY
 });
 
+app.get("/test", function(req, res) {
+  res.send(APIrespone);
+});
+/* 
 textapi.sentiment(
   {
-    text: "John is a very good football player!"
+    text: "I do not like his atitude"
   },
   function(error, response) {
     if (error === null) {
-      console.log(response);
+      const { polarity, subjectivity } = response;
+      APIrespone.polarity = polarity;
+      APIrespone.subjectivity = subjectivity;
+      console.log(APIrespone);
     }
   }
-);
+); */
